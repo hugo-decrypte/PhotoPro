511f18db65bc2d695577544e78793ff4a92061ed
Hugo Bena <heno.drawing@gmail.com>
2026-04-01 17:27:03 +0200

    UPDATE : placement auth service

---
+++ api/app-auth/src/api/dto/auth/CredentialsDTO.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace toubilib\api\dto\auth;
+
+class CredentialsDTO{
+
+    private string $email;
+    private string $password;
+
+    public function __construct(string $email, string $password)
+    {
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+
+}
