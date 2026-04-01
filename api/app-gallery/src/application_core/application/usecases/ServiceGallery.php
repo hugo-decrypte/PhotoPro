fa6ea16c9b0c8835cd1b12d40d3744fd785ae0f7
Riole88 <boullemanitou@gmail.com>
2026-04-01 10:15:22 +0200

    Update ServiceGallery.php

--- api/app-gallery/src/application_core/application/usecases/ServiceGallery.php
+++ api/app-gallery/src/application_core/application/usecases/ServiceGallery.php
@@ -8,29 +8,11 @@ use photopro\core\application\ports\spi\repositoryInterfaces\GalleryRepositoryIn
 
 class ServiceGallery implements ServiceGalleryInterface
 {
-    private PraticienRepositoryInterface $praticienRepository;
     private GalleryRepositoryInterface $galleryRepository;
 
-    public function __construct(PraticienRepositoryInterface $praticienRepository, GalleryRepositoryInterface $galleryRepository)
+    public function __construct(GalleryRepositoryInterface $galleryRepository)
     {
-        $this->praticienRepository = $praticienRepository;
         $this->galleryRepository = $galleryRepository;
-    }
-
-    public function listerPraticiens(): array
-    {
-        $praticiens = $this->praticienRepository->findAll();
-        
-        return array_map(function ($praticien) {
-            return new PraticienDTO(
-                id: $praticien->getId()->toString(),
-                nom: $praticien->getNom(),
-                prenom: $praticien->getPrenom(),
-                ville: $praticien->getVille(),
-                email: $praticien->getEmail(),
-                specialite: $praticien->getSpecialiteLibelle() // Cette méthode sera ajoutée à l'entité
-            );
-        }, $praticiens);
     }
 
     public function listOfGalery(): array
