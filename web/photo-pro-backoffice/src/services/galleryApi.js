97642fc389a232fb978e964f5c5d79e42a1ea1e0
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-04-03 11:34:02 +0200

    VueJS (/gallery)

--- web/photo-pro-backoffice/src/services/galleryApi.js
+++ web/photo-pro-backoffice/src/services/galleryApi.js
@@ -1,6 +1,15 @@
 import http from '../lib/http'
 import { normalizeApiError } from '../lib/apiError'
 
+export async function fetchGalleries() {
+  try {
+    const { data } = await http.get('/galeries')
+    return data
+  } catch (error) {
+    throw normalizeApiError(error)
+  }
+}
+
 export async function publishGallery(galleryId) {
   try {
     const { data } = await http.patch(`/galeries/${encodeURIComponent(galleryId)}/publish`)
