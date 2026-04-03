02eac5535947a68124302f4c8a532298a19b9072
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-04-03 10:24:34 +0200

    vueJS - inscription champs supplémentaire + navbar

--- web/photo-pro-backoffice/src/router/index.js
+++ web/photo-pro-backoffice/src/router/index.js
@@ -7,6 +7,7 @@ import GalleryEditView from '../views/GalleryEditView.vue'
 import LoginView from '../views/LoginView.vue'
 import PhotosView from '../views/PhotosView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import SettingsView from '../views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,6 +43,11 @@ const router = createRouter({
       path: '/gallery/new',
       name: 'gallery-new',
       component: GalleryEditView,
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
     },
   ],
 })
