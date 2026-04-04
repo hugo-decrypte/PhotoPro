3c9500587928cc5f38ca094443de0f82642abe8d
tuline-leveque <tuline.leveque@gmail.com>
2026-04-04 17:48:04 +0200

    ajout adaptateur pour le service auth dans gallery + modification des fonctions addComment, unpublishGallery, publishGallery et createGallery pour envoyer des mails

--- api/app-gallery/config/services.php
+++ api/app-gallery/config/services.php
@@ -3,11 +3,32 @@
 use DI\Container;
 use photopro\core\application\ports\api\ServiceGalleryInterface;
 use photopro\core\application\ports\spi\repositoryInterfaces\GalleryRepositoryInterface;
+use photopro\core\application\ports\spi\repositoryInterfaces\MailSenderInterface;
+use photopro\core\application\ports\spi\repositoryInterfaces\ServiceAuthAdaptatorInterface;
 use photopro\core\application\usecases\ServiceGallery;
+use photopro\infra\adapter\ServiceAuthAdaptor;
+use photopro\infra\repositories\MailSender;
+use Psr\Container\ContainerInterface;
 
 return [
+    'auth.db.config' => __DIR__ . '/.auth.env',
+
+    'auth.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file($c->get('auth.db.config'));
+        $dsn = "{$config['auth.driver']}:host={$config['auth.host']};dbname={$config['auth.database']}";
+        $user = $config['auth.username'];
+        $password = $config['auth.password'];
+        return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+    },
     ServiceGalleryInterface::class => function (Container $container) {
-        $repository = $container->get(GalleryRepositoryInterface::class);
-        return new ServiceGallery($repository);
+        return new ServiceGallery($container->get(GalleryRepositoryInterface::class),$container->get(ServiceAuthAdaptatorInterface::class),$container->get(MailSenderInterface::class));
+    },
+
+    ServiceAuthAdaptatorInterface::class => function (Container $c) {
+        return new ServiceAuthAdaptor($c->get("auth.pdo"));
+    },
+
+    MailSenderInterface::class => function (ContainerInterface $c) {
+        return new MailSender();
     },
 ];
