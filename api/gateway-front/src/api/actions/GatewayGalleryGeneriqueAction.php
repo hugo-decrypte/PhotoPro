a7004111f90f385c14aa4ef19e81d4cb1cd0e50b
Riole88 <boullemanitou@gmail.com>
2026-04-07 11:54:37 +0200

    update avoir l'info et les photos des gallery privée

--- api/gateway-front/src/api/actions/GatewayGalleryGeneriqueAction.php
+++ api/gateway-front/src/api/actions/GatewayGalleryGeneriqueAction.php
@@ -86,6 +86,12 @@ class GatewayGalleryGeneriqueAction
             $options['headers']['X-Photographer-Id'] = $photographerHeader;
         }
 
+        // Query parameters
+        $queryParams = $request->getQueryParams();
+        if (!empty($queryParams)) {
+            $options['query'] = $queryParams;
+        }
+
         if (!empty($body) && in_array($method, ['POST', 'PUT', 'PATCH'])) {
             if (is_array($body) || is_object($body)) {
                 $options['json'] = $body;
