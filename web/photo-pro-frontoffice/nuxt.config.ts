19a4ee2aeecaad95fdea2ff1b122f1132fc7d706
Hugo Bena <heno.drawing@gmail.com>
2026-04-07 14:22:05 +0200

    INIT du projet nuxt + page principale, les fetch ne fonctionnent pas encore

---
+++ web/photo-pro-frontoffice/nuxt.config.ts
@@ -0,0 +1,44 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  ssr: true,
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }))
+      })
+    },
+  ],
+
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+
+css: [
+    'vuetify/styles',
+    '@mdi/font/css/materialdesignicons.css',
+    '~/assets/css/main.css'
+    ],
+
+  runtimeConfig: {
+    dbHost: '',
+    dbPort: '',
+    dbUser: '',
+    dbPassword: '',
+    dbName: '',
+    authApiUrl: '',
+    galleryApiUrl: '',
+    public: {
+      s3Endpoint: '',
+    }
+  },
+})
